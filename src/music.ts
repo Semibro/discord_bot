c905ab38d2f95e53f14d83de622ee2ef86a9a29b
Semibro <wnsgud6232@naver.com>
2025-07-17 13:41:58 +0900

    FIX: yt-dlp options

--- src/music.ts
+++ src/music.ts
@@ -95,9 +95,10 @@ const playNextTrack = async (guildId: string): Promise<void> => {
         const ytDlpArgs = [
             '--no-check-certificate',
             '--add-header', 'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            '--force-ipv4',
             '-q',
             '--no-warnings',
-            '-f', 'bestaudio',
+            '-f', 'bestaudio/best',
             '--extract-audio',
             '--audio-format', 'mp3',
             '--output', '-',
